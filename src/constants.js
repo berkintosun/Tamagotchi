84fc9543146fd46919faa4f21bab68e5950401cc
Berkin Tosun <berkintsn@gmail.com>
2020-11-28 07:25:55 +0200

    updated constants

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,11 @@
-export const TICK_RATE = 1000;
+export const TICK_RATE = 3000;
 export const ICONS = ["fish", "poop", "weather"];
 export const RAIN_CHANCE = 0.3;
 export const SCENES = ["day", "rain"];
 export const DAY_LENGTH = 20;
 export const NIGHT_LENGTH = 3;
 export const getNextHungerTime = (clock) =>
-  Math.floor(Math.random() * 4) + 4 + clock;
+  Math.floor(Math.random() * 3) + 5 + clock;
 export const getNextDieTime = (clock) =>
   Math.floor(Math.random() * 2) + 3 + clock;
 export const getNextPoopTime = (clock) =>
